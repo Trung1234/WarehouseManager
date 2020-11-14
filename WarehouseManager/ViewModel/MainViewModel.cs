00b8008d7ea62df68d68908861d7bd66a75fd100
unknown <trungsieunhan24@gmail.com>
2020-11-14 06:55:54 -0800

    first commit

---
+++ WarehouseManager/ViewModel/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WarehouseManager.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        public bool IsLoaded = false;
+        public MainViewModel()
+        {
+            if (!IsLoaded)
+            {
+                IsLoaded = true;
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.ShowDialog();
+            }           
+        }
+    }
+}
