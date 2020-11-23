d256f19e76c6d845ed5857ba6768f365a7d13410
unknown <trungsieunhan24@gmail.com>
2020-11-23 04:58:12 -0800

    create model layer

---
+++ WarehouseManager/SQLScript/script.sql
@@ -0,0 +1,226 @@
+USE [master]
+GO
+/****** Object:  Database [Warehouse]    Script Date: 26-Feb-18 6:29:19 PM ******/
+CREATE DATABASE [Warehouse]
+GO
+USE [Warehouse]
+GO
+/****** Object:  Table [dbo].[Customer]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Customer](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[Address] [nvarchar](max) NULL,
+	[Phone] [nvarchar](20) NULL,
+	[Email] [nvarchar](200) NULL,
+	[MoreInfo] [nvarchar](max) NULL,
+	[ContractDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Input]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Input](
+	[Id] [nvarchar](128) NOT NULL,
+	[DateInput] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[InputInfo]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[InputInfo](
+	[Id] [nvarchar](128) NOT NULL,
+	[IdObject] [nvarchar](128) NOT NULL,
+	[IdInput] [nvarchar](128) NOT NULL,
+	[Count] [int] NULL,
+	[InputPrice] [float] NULL,
+	[OutputPrice] [float] NULL,
+	[Status] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Object]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Object](
+	[Id] [nvarchar](128) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[IdUnit] [int] NOT NULL,
+	[IdSuplier] [int] NOT NULL,
+	[QRCode] [nvarchar](max) NULL,
+	[BarCode] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Output]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Output](
+	[Id] [nvarchar](128) NOT NULL,
+	[DateOutput] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[OutputInfo]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[OutputInfo](
+	[Id] [nvarchar](128) NOT NULL,
+	[IdObject] [nvarchar](128) NOT NULL,
+	[IdOutputInfo] [nvarchar](128) NOT NULL,
+	[IdCustomer] [int] NOT NULL,
+	[Count] [int] NULL,
+	[Status] [nvarchar](max) NULL,
+
+	foreign key (Id) references OutPut(Id),
+
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Suplier]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Suplier](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[Address] [nvarchar](max) NULL,
+	[Phone] [nvarchar](20) NULL,
+	[Email] [nvarchar](200) NULL,
+	[MoreInfo] [nvarchar](max) NULL,
+	[ContractDate] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Unit]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Unit](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[UserRole]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[UserRole](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 26-Feb-18 6:29:19 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[DisplayName] [nvarchar](max) NULL,
+	[UserName] [nvarchar](100) NULL,
+	[Password] [nvarchar](max) NULL,
+	[IdRole] [int] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+SET IDENTITY_INSERT [dbo].[UserRole] ON 
+
+INSERT [dbo].[UserRole] ([Id], [DisplayName]) VALUES (1, N'Admin')
+INSERT [dbo].[UserRole] ([Id], [DisplayName]) VALUES (2, N'Nhân viên')
+SET IDENTITY_INSERT [dbo].[UserRole] OFF
+SET IDENTITY_INSERT [dbo].[Users] ON 
+
+INSERT [dbo].[Users] ([Id], [DisplayName], [UserName], [Password], [IdRole]) VALUES (1, N'RongK9', N'admin', N'db69fc039dcbd2962cb4d28f5891aae1', 1)
+INSERT [dbo].[Users] ([Id], [DisplayName], [UserName], [Password], [IdRole]) VALUES (2, N'Nhân viên', N'staff', N'978aae9bb6bee8fb75de3e4830a1be46', 2)
+SET IDENTITY_INSERT [dbo].[Users] OFF
+ALTER TABLE [dbo].[InputInfo] ADD  DEFAULT ((0)) FOR [InputPrice]
+GO
+ALTER TABLE [dbo].[InputInfo] ADD  DEFAULT ((0)) FOR [OutputPrice]
+GO
+ALTER TABLE [dbo].[InputInfo]  WITH CHECK ADD FOREIGN KEY([IdInput])
+REFERENCES [dbo].[Input] ([Id])
+GO
+ALTER TABLE [dbo].[InputInfo]  WITH CHECK ADD FOREIGN KEY([IdObject])
+REFERENCES [dbo].[Object] ([Id])
+GO
+ALTER TABLE [dbo].[Object]  WITH CHECK ADD FOREIGN KEY([IdUnit])
+REFERENCES [dbo].[Unit] ([Id])
+GO
+ALTER TABLE [dbo].[Object]  WITH CHECK ADD FOREIGN KEY([IdSuplier])
+REFERENCES [dbo].[Suplier] ([Id])
+GO
+ALTER TABLE [dbo].[OutputInfo]  WITH CHECK ADD FOREIGN KEY([IdCustomer])
+REFERENCES [dbo].[Customer] ([Id])
+GO
+
+ALTER TABLE [dbo].[OutputInfo]  WITH CHECK ADD FOREIGN KEY([IdObject])
+REFERENCES [dbo].[Object] ([Id])
+GO
+ALTER TABLE [dbo].[Users]  WITH CHECK ADD FOREIGN KEY([IdRole])
+REFERENCES [dbo].[UserRole] ([Id])
+GO
+USE [master]
+GO
+ALTER DATABASE [Warehouse] SET  READ_WRITE 
+GO
