997edfd8d33c17a5d20a9f2ee9df233c59fc6501
unknown <trungsieunhan24@gmail.com>
2020-11-22 18:51:36 -0800

    add more windows

--- WarehouseManager/ViewModel/MainViewModel.cs
+++ WarehouseManager/ViewModel/MainViewModel.cs
@@ -12,11 +12,20 @@ namespace WarehouseManager.ViewModel
     {
         public bool Isloaded = false;
         public ICommand LoadedWindowCommand { get; set; }
+        public ICommand UnitCommand { get; set; }
+        public ICommand SuplierCommand { get; set; }
         public ICommand CustomerCommand { get; set; }
+        public ICommand ObjectCommand { get; set; }
+        public ICommand UserCommand { get; set; }
+        public ICommand InputCommand { get; set; }
+        public ICommand OutputCommand { get; set; }
 
-        // mọi thứ xử lý sẽ nằm trong này
+        /// <summary>
+        /// logic events for MainViewdow
+        /// </summary>
         public MainViewModel()
         {
+            CustomerWindow cwd = null;
             LoadedWindowCommand = new RelayCommand<object>((p) => { return true; }, (p) => {
                 Isloaded = true;
                 LoginWindow loginWindow = new LoginWindow();
@@ -25,9 +34,47 @@ namespace WarehouseManager.ViewModel
            );
             CustomerCommand = new RelayCommand<object>((p) => { return true; }, (p) => 
             { 
-                CustomerWindow wd = new CustomerWindow();
-                wd.ShowDialog(); 
+                cwd = new CustomerWindow();
+                cwd.ShowDialog(); 
+            });
+            InputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                InputWindow wd = new InputWindow();
+                wd.ShowDialog();
+            });
+            OutputCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                OutputWindow wd = new OutputWindow();
+                wd.ShowDialog();
+            });
+
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+            UnitCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UnitWindow wd = new UnitWindow();
+                wd.ShowDialog();
+            });
+            SuplierCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                SuplierWindow wd = new SuplierWindow();
+                wd.ShowDialog();
+            });
+            ObjectCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                ObjectWindow wd = new ObjectWindow();
+                wd.ShowDialog();
+            });
+            UserCommand = new RelayCommand<object>((p) => { return true; }, (p) =>
+            {
+                UserWindow wd = new UserWindow();
+                wd.ShowDialog();
             });
         }
+
+
     }
 }
