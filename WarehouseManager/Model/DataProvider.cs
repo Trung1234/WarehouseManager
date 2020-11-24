ef438bcfefca30bbfefc1038c3727a9398cea829
unknown <trungsieunhan24@gmail.com>
2020-11-24 05:26:31 -0800

    create data provider

---
+++ WarehouseManager/Model/DataProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WarehouseManager.Model
+{
+    public class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                    _ins = new DataProvider();
+                return _ins;
+            }
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public WarehouseEntities DB { get; set; }
+
+        private DataProvider()
+        {
+            DB = new WarehouseEntities();
+        }
+    }
+}
